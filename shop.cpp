0c8f3d5994a097145f4b75574d1fe1a85f6077f9
Poloeli303 <91678247+Poloeli303@users.noreply.github.com>
2021-11-16 09:58:55 -0700

    Update shop.cpp

--- shop.cpp
+++ shop.cpp
@@ -1,34 +1,41 @@
 #include<iostream>
 using namespace std;
+int health = 100;
+int money = 500;
 int main() {
 	string inventory[10];
 	string input;
 	cout << "Welcome player! How are you?" << endl << endl;
 	while (input != "q") {
-	cout << "This is my shop. You can buy potions." << endl;
-	cout << "To buy a healing potion type 'h', to buy a speed potion type 's', to buy a jump potion type 'j', and to buy a flying potion type 'f'. Press i for inventory. Press 'q' to leave the shop." << endl;
-	cin >> input;
-	if (input.compare("h") == 0) {
-		inventory[0] = "Healing potion";
-		cout << "You bought a healing potion!" << endl << endl;
-	}
+		cout << "money:" << money << endl;
+		cout << "This is my shop. You can buy potions." << endl;
+		cout << "To buy a healing potion type 'h', to buy a speed potion type 's', to buy a jump potion type 'j', and to buy a flying potion type 'f'. Press i for inventory. Press 'q' to leave the shop." << endl;
+		cin >> input;
+		if (input.compare("h") == 0) {
+			inventory[0] = "Healing potion";
+			cout << "You bought a healing potion!" << endl << endl;
+			money -= 50;
+		}
 		else if (input.compare("s") == 0) {
 			inventory[1] = "Speed potion";
 			cout << "You bought a speed potion!" << endl << endl;
+			money -= 50;
 		}
 		else if (input.compare("j") == 0) {
-		inventory[2] = "Jump potion";
-		cout << "You bought a jump potion!" << endl << endl;
+			inventory[2] = "Jump potion";
+			cout << "You bought a jump potion!" << endl << endl;
+			money -= 50;
 		}
 		else if (input.compare("f") == 0) {
-		inventory[3] = "Flying potion";
-		cout << "You bought a flying potion!" << endl << endl;
+			inventory[3] = "Flying potion";
+			cout << "You bought a flying potion!" << endl << endl;
+			money -= 50;
 		}
 
 		else if (input == "i") {
 			cout << "Here is your inventory:" << endl;
 			for (int i = 0; i < 10; i++)
-				cout << inventory[i]<<endl;
+				cout << inventory[i] << endl;
 			cout << endl << endl;
 		}
 
